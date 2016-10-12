8ee6407943da6aeefec9b24362c501c9545c9940
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2016-10-12 14:44:25 -0400

    Optimize Radar_Object.ScanRadar
    Profiling shows that a significant amount of computation time each frame
    is spent inside the Radar_Object.ScanRadar function. This commit makes
    some optimizations to the function to reduce the overhead per frame.

--- Radar.py
+++ Radar.py
@@ -14,27 +14,26 @@ class Radar_Object(object):
         self.image.fill((255,255,255))
         self.image.set_colorkey((255,255,255))
         self.degreeStep = 1
-        self.RadarData = np.ones(360 / int(self.degreeStep))
 
-    def ScanRadar(self, CoordinateofCenter , Grid_Data):
+    def ScanRadar(self, CoordinateofCenter, Grid_Data):
 
-        self.RadarData = np.ones(360 / int(self.degreeStep))
+        radar_data = np.ones(360 / int(self.degreeStep))
         currentStep = 0
-        for degree in np.arange(0, 360 , self.degreeStep):
+        x_upper_bound = min(799, self.screen.get_width())
+        y_upper_bound = min(599, self.screen.get_height())
+        for degree in np.arange(0, 360, self.degreeStep):
+            ang_in_radians = degree * math.pi / 180
+            cos_cached = math.cos(ang_in_radians)
+            sin_cached = math.sin(ang_in_radians)
             for i in range(0, self.RadarRadius, 10):
-                x = math.cos(degree * math.pi / 180) * i
-                y = math.sin(degree * math.pi / 180) * i
-                x = int(CoordinateofCenter[0] + x)
-                y = int(CoordinateofCenter[1] + y)
-                if ((x < 0) or (y < 0) or (x >= self.screen.get_width()) or (y >= self.screen.get_height())):
-                    self.RadarData[currentStep] = i / self.RadarRadius
-                    continue
-                if (x > 799 or y > 599):
-                    self.RadarData[currentStep] = i / self.RadarRadius
-                    continue
-                if (Grid_Data[x,y] == 1 ):
-                    self.RadarData[currentStep] = i / self.RadarRadius
+                x = int(cos_cached * i + CoordinateofCenter[0])
+                y = int(sin_cached * i + CoordinateofCenter[1])
+                if ((x < 0) or (y < 0) or (x_upper_bound <= x) or (y_upper_bound <= y)):
+                    radar_data[currentStep] = i / self.RadarRadius
+                    break
+                if (Grid_Data[x,y] == 1):
+                    radar_data[currentStep] = i / self.RadarRadius
                     break
                 #self.screen.set_at((x, y), self.RadarColor)
             currentStep = currentStep + 1
-        return self.RadarData
+        return radar_data
