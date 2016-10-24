b23532d45a6ef3e7977efb3452adbddfb88354c2
Arash Roshanineshat <Arash1234@users.noreply.github.com>
2016-10-24 14:09:18 -0400

    Add files via upload

--- Game.py
+++ Game.py
@@ -8,23 +8,23 @@ from  Robot       import Robot_Object
 from  Target      import Target_Object
 from  Monitoring  import Monitors
 import time
-
 class Game_Object(object):
     def __init__(self, cmdargs):
         PG.init()
         self.StaticObstacles = []
 
-        self.cmdargs            = cmdargs
+        self.cmdargs               = cmdargs
         self.WhiteColor         = (255, 255, 255)
         self.BlackColor         = (0, 0, 0)
         self.Display_Width      = 1200
         self.Display_Height     = 600
+        self.MainWindow_Title   = 'Robot Simulator'
+        PG.display.set_caption (self.MainWindow_Title)
         self.gameDisplay        = PG.display.set_mode((self.Display_Width, self.Display_Height))
-        self.Playground         = Playground_Object(800, self.Display_Height, cmdargs.map_name, cmdargs=cmdargs)
+        self.Playground         = Playground_Object(800, self.Display_Height, cmdargs.map_name)
         self.TargetPoint        = Target_Object((740,50))
-        self.Robot              = Robot_Object (self.gameDisplay, self.TargetPoint,(50,550), speed=cmdargs.robot_speed, cmdargs=cmdargs)
-        self.Monitor            = Monitors(self.Robot, PG.display, self.Playground.Playground)
-        PG.display.set_caption('Robot Simulator')
+        self.Normal_Robot       = Robot_Object (self.gameDisplay, self.TargetPoint,(50,550), speed=cmdargs.robot_speed, cmdargs=cmdargs)
+        self.Safe_Robot         = Robot_Object (self.gameDisplay, self.TargetPoint,(50,550), speed=cmdargs.robot_speed, cmdargs=cmdargs, issafe = True)
 
     def GameLoop(self):
         clock = PG.time.Clock()
@@ -34,26 +34,30 @@ class Game_Object(object):
             for event in PG.event.get():
                 if event.type == PG.QUIT:
                     shouldClose = True
-                if event.type == PG.MOUSEBUTTONUP:
-                    pos = PG.mouse.get_pos()
-                    self.Robot.location = pos
-                    self.Robot.PathList.append(pos)
+                #if event.type == PG.MOUSEBUTTONUP:
+                #    pos = PG.mouse.get_pos()
+                #    self.Robot.location = pos
+                #    self.Robot.PathList.append(pos)
+                    # if event.type == PG.KEYDOWN:
             self.Playground.Nextstep(self.gameDisplay)
             clockrobot = 0
 
             if self.cmdargs.fast_computing:
-                self.Robot.Coordinate = (50,550)
+                self.Normal_Robot.Coordinate = (50,550)
+                self.Safe_Robot.Coordinate = (50,550)
                 isAtTarget = False
-                self.Robot.PathList.clear()
+                self.Normal_Robot.PathList.clear()
+                self.Safe_Robot.PathList.clear()
                 while (not isAtTarget):
-                    isAtTarget = self.Robot.NextStep(self.Playground.GridData)
+                    isAtTarget = self.Normal_Robot.NextStep(self.Playground.GridData) and self.Safe_Robot.NextStep(self.Playground.GridData)
                     clockrobot = clockrobot + 1
             else:
-                self.Robot.NextStep(self.Playground.GridData)
+                self.Normal_Robot.NextStep(self.Playground.GridData)
+                self.Safe_Robot.NextStep(self.Playground.GridData)
 
             self.TargetPoint.draw(self.gameDisplay)
-            self.Robot.draw(self.gameDisplay)
-            self.Monitor.draw()
+            self.Normal_Robot.draw(self.gameDisplay)
+            self.Safe_Robot.draw(self.gameDisplay)
             PG.display.update()
 #            print (clockrobot * 5)
 #            print ("Distance : ")
--- Robot.py
+++ Robot.py
@@ -1,25 +1,22 @@
 import numpy  as np
 import pygame as PG
 import math
-from Radar import Radar_Object
+from Radar import  Radar_Object
 import Distributions
 import Vector
 import matplotlib.pyplot as plt
 import time
 
 class Robot_Object(object):
-    def __init__(self, screen,  Target_Object, StartLocation, speed = 3, cmdargs=None):
+    def __init__(self, screen,  Target_Object, StartLocation, speed = 3, cmdargs=None, issafe = False):
         self.cmdargs            = cmdargs
-        self.TargetObj          = Target_Object
         self.location           = StartLocation
+        self.TargetObj          = Target_Object
         self.speed              = speed
-
-        radar_resolution = cmdargs.radar_resolution if cmdargs else 4
-
-        self.radar              = Radar_Object(screen, resolution=radar_resolution)
+        self.radar              = Radar_Object(screen)
         self.PathList           = []
         self.PDF                = Distributions.Distributions()
-
+        self.IAmSafe            = issafe
         # Function that selects an angle based on a distribution
         self.pdf_angle_selector = self.center_of_gravity_pdfselector
         # Function that combines pdfs
@@ -44,12 +41,14 @@ class Robot_Object(object):
         self.speed = speed
 
 
-    def Gaussian_noise(self, pdf, noise_level):
-        noise_pdf = np.random.normal(0, noise_level, pdf.size) + pdf
+    def Gaussian_noise(self, pdf):
+        noise_pdf = np.random.normal(0, 0.02, pdf.size) + pdf
         return noise_pdf
 
 
     def NextStep(self, grid_data):
+        if (self.distanceToTarget() < 20):
+            return True
         self.stepNum += 1
 
         targetpoint_pdf = self.PDF.Radar_GaussianDistribution(self.angleToTarget())
@@ -58,14 +57,10 @@ class Robot_Object(object):
         elif (not (self.cmdargs is None)) and (self.cmdargs.target_distribution_type == 'dotproduct'):
             targetpoint_ang = self.angleToTarget()
             targetpoint_pdf = np.array([(self.speed*(np.cos(np.abs(targetpoint_ang - ang) * np.pi/180)+1)/2) for ang in np.arange(0, 360, self.PDF.DegreeResolution)], dtype='float64')
-            targetpoint_pdf = (targetpoint_pdf / np.amax(targetpoint_pdf)) * 0.8 + 0.2
+            targetpoint_pdf = (targetpoint_pdf/np.amax(targetpoint_pdf)) * 0.8 + 0.2
 
         self.combined_pdf = targetpoint_pdf
-
-        RadarData = self.radar.ScanRadar(self.location, grid_data)
-        if (self.cmdargs) and not (self.cmdargs.radar_noise_level <= 0):
-            RadarData = self.Gaussian_noise(RadarData, self.cmdargs.radar_noise_level)
-
+        RadarData = self.Gaussian_noise(self.radar.ScanRadar(self.location, grid_data))
         self.combined_pdf = self.combine_pdfs(self.combined_pdf, RadarData)
 
         if (not (self.cmdargs is None)) and (self.cmdargs.enable_memory):
@@ -75,20 +70,15 @@ class Robot_Object(object):
 
             mem_bias_ang = Vector.getAngleBetweenPoints((0,0), mem_bias_vec)
             mem_bias_mag = Vector.getDistanceBetweenPoints((0,0), mem_bias_vec)
-
-            # Create memory distribution based on dot product with memory vector
-            mem_bias_pdf = np.array([np.cos(np.abs(mem_bias_ang - ang) * np.pi/180) for ang in np.arange(0, 360, self.PDF.DegreeResolution)])
-            mem_bias_pdf += 1 # Add 1 to get the cosine function above 0
+            
+            mem_bias_pdf = np.array([(np.cos(np.abs(mem_bias_ang-ang) * np.pi/180)) for ang in np.arange(0, 360, self.PDF.DegreeResolution)])
             if np.amax(mem_bias_pdf) > 0:
-                mem_bias_pdf = mem_bias_pdf / np.amax(mem_bias_pdf)
+                mem_bias_pdf = mem_bias_pdf / np.amax(mem_bias_pdf) * 0.75
+            mem_bias_pdf = mem_bias_pdf + 0.25
             self.combined_pdf = self.combine_pdfs(self.combined_pdf, mem_bias_pdf)
 
             if (self.stepNum % 1) == 0:
                 self.visited_points.append(self.location)
-
-        # Bias the distribution to stay away from obstacles
-        self.combined_pdf[RadarData>0.7] *= 1.1
-        self.combined_pdf[RadarData < 0.2] -= 1
 
         movement_ang = self.pdf_angle_selector(self.combined_pdf) * self.PDF.DegreeResolution
 
@@ -103,19 +93,17 @@ class Robot_Object(object):
 
         movement_vec = np.array([np.cos(movement_ang * np.pi / 180), np.sin(movement_ang * np.pi / 180)], dtype='float64') * self.speed
         self.last_mmv = movement_vec
-
+        movement_vec = np.array(movement_vec, dtype=int)
         new_location = np.add(self.location, movement_vec)
-        if (grid_data[int(new_location[0]), int(new_location[1])] == 1):
+
+        if not (grid_data[int(new_location[0]), int(new_location[1])] == 1):
+            self.location = new_location
+        else:
             print('Robot glitched into obstacle!')
             self.location = np.add(self.location, -movement_vec*1.5)
 
-        if (self.cmdargs) and (self.cmdargs.use_integer_robot_location):
-            new_location = np.array(new_location, dtype=int)
-        self.location = new_location
-
         self.PathList.append(np.array(self.location, dtype=int))
-        if (self.distanceToTarget() < 20):
-            return True
+
         return False
 
 
@@ -124,11 +112,22 @@ class Robot_Object(object):
         maxval_ind = np.argmax(pdf)
         sum_num = 0
         sum_den = 0
-        for n in np.arange(int(maxval_ind - width / 2), int(maxval_ind + width/2), 1):
-            sum_num += pdf[n % 360] * n
-            sum_den += pdf[n % 360]
-        center_of_mass = int(sum_num / (sum_den + 0.00001)) # Add a small number (0.00001) in case sum_den is 0
-        return center_of_mass % 360
+        minrange = int(maxval_ind - width / 2)
+        maxrange = int(maxval_ind + width/2)
+        for n in np.arange(minrange, maxrange , 1 ):
+            ind = n
+            if n < 0: 
+                ind = n + 360
+            if n > 359:
+                ind = n - 360
+            sum_num += pdf[ind]*n
+            sum_den += pdf[ind]
+        center_of_mass = int(sum_num / (sum_den + 0.00001)) # Add a small number (0.0001) in case sum_den is 0          
+        if center_of_mass < 0: 
+            center_of_mass += 360
+        if center_of_mass > 359:
+            center_of_mass -= 360
+        return center_of_mass
 
 
     def threshold_midpoint_pdfselector(self, pdf):
@@ -158,8 +157,8 @@ class Robot_Object(object):
 
 
     def calc_memory_bias_vector(self):
-        sigmaSquared = 15 ** 2
-        gaussian_derivative = lambda x: -x*(np.exp(-(x*x/(2*sigmaSquared))) / sigmaSquared)
+        sigmaSquared = 10 ** 2
+        gaussian_derivative = lambda x: -0.1*x*(np.exp(-(x*x/(2*sigmaSquared))) / sigmaSquared)
         vec = np.array([0, 0], dtype='float64')
         for point in self.visited_points:
         #for point in [PG.mouse.get_pos()]:
@@ -173,10 +172,17 @@ class Robot_Object(object):
         # Be careful when uncommenting things in this function.
         # Each line drawn makes it more likely that the robot goes through obstacles
         #PG.draw.circle(screen, (0, 0, 255), np.array(self.location, dtype=int), 4, 0)
+        BlueColor  = (0,0,255)
+        GreenColor = (10, 100,10)
+        if (self.IAmSafe):
+            PathColor = GreenColor
+        else:
+            PathColor = BlueColor
         for ind, o in enumerate(self.PathList):
             if ind == len(self.PathList) - 1:
                 continue
-            PG.draw.line(screen,(0,0,255),self.PathList[ind], self.PathList[ind +1], 3)
+
+            PG.draw.line(screen,PathColor,self.PathList[ind], self.PathList[ind +1], 3)
         if (not (self.cmdargs is None)) and (0 < self.cmdargs.debug_level):
             # Draw line representing memory effect
             #PG.draw.line(screen, (0,255,0), np.array(self.location, dtype=int), np.array(self.location+self.last_mbv*100, dtype=int), 1)
@@ -185,7 +191,7 @@ class Robot_Object(object):
             #PG.draw.line(screen, (255,0,0), np.array(self.location, dtype=int), np.array(self.location+self.last_mmv*100, dtype=int), 1)
 
             # Draw circle representing radar range
-            PG.draw.circle(screen, (255,255,0), np.array(self.location, dtype=int), self.radar.RadarRadius, 2)
+            PG.draw.circle(screen, PathColor, np.array(self.location, dtype=int), self.radar.RadarRadius, 2)
 
             # Draw distribution values around robot
             #self.draw_pdf(screen, self.combined_pdf)
