37265258912dde28161c5e92897714a621cf6033
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-01-08 13:57:41 -0500

    Add documentation
    This commit adds a lot of documentation for various classes. It also
    changes the visibility of some class members and changes some method
    signitures, to improve self-documentation of code.

--- DynamicObstacles.py
+++ DynamicObstacles.py
@@ -3,17 +3,19 @@ import pygame as PG
 import Vector
 
 
+## Represents a dynamic obstacle
+#
 class DynamicObstacle:
 
 	def __init__(self):
-		self.radius		= 0
-		self.coordinate		= np.array([0, 0])
-		self.origin		= np.array([0, 0])
-		self.size		= [50, 50]
+		self.radius		= 0 # Used for Circle shape
+		self.coordinate		= np.array([0, 0]) # Location
+		self.origin		= np.array([0, 0]) # Point around which the obstacle rotates for circular motion
+		self.size		= [50, 50] # Used for Rectangle shape
 		self.fillcolor		= (0x22, 0x77, 0x22)
 		self.bordercolor	= (0xFF, 0x00, 0x00)
 		self.movement_mode	= 1
-		self.shape		= 1
+		self.shape		= 1 # 1 = Circle, 2 = Rectangle
 		self.speed		= 8
 		self.tempind		= 1
 		self.path_list		= []
@@ -25,6 +27,19 @@ class DynamicObstacle:
 	def set_radius(self, radius):
 		self.radius = radius
 
+	## Does a motion step for this obstacle, updating its position and
+	# direction according to its movement mode.
+	#
+	# There are three movement modes defined as follows:
+	# <br>	Mode 1. Fully random motion. The obstacle picks a new
+	# 	direction on each step.
+	# <br>	Mode 2. Circular motion. The obstacle moves in a circle of
+	# 	radius 30. Note that the `radius` field of the obstacle
+	# 	relates to its shape, and NOT to this motion.
+	# <br>	Mode 3. Moving along a path. The obstacle moves towards the
+	# 	next point in its `path_list` field. When the point is
+	# 	reached, it selects the next point and moves towards that
+	# 	one.
 	def next_step(self):
 		if (self.movement_mode == 1):
 			self.coordinate = np.add(self.coordinate, np.random.uniform(-self.speed, self.speed, size=[2]))
--- Radar.py
+++ Radar.py
@@ -5,8 +5,31 @@ import Vector, Geometry
 import math
 
 
+## Produces simulated radar output for the robot
+#
+# This class simulates a radar device. Using information from the
+# environment, it produces a radar scan that shows the relative distance to
+# obstacles.
+#
 class Radar:
 
+	## Constructor
+	#
+	# @param env (Environment object)
+	# <br>	-- The environment (holds the information about the
+	# 	obstacles for scanning)
+	#
+	# @param radius (float)
+	# <br>	-- The range of the radar. Obstacles beyond this distance
+	# 	will not be detected when scanning.
+	#
+	# @param resolution (int)
+	# <br>	-- The resolution used for sampling-based scanning.
+	# 
+	# @param degree_step (float)
+	# <br>	-- The increment for the scanning beam. This controls how
+	# 	many different angles are checked for obstacles.
+	#
 	def __init__(self, env, radius = 100, resolution = 4, degree_step = 1):
 
 		self.env         = env
@@ -15,6 +38,31 @@ class Radar:
 		self.set_degree_step(degree_step);
 
 
+	## Produces a radar scan
+	#
+	# @param center (numpy array)
+	# <br>	Format `[x, y]`
+	# <br>	-- The center point of the scan
+	#
+	# @param grid_data (2D numpy array)
+	# <br>	Format: 2D grid with values: 0 for no obstacle, 1 for
+	# 	static obstacle, 3 for dynamic obstacle. This works like a
+	# 	bitmask where the lowest bit indicates the presence of an
+	# 	obstacle and the second-lowest bit indicates movement. The
+	# 	array should be arranged to `grid_data[x, y]` corresponds
+	# 	to the same `x` and `y` in the environment.
+	# <br>	-- Occupancy grid to indicate where obstacles are
+	#
+	# @returns (numpy array)
+	# <br>	Format: `[ang1_val, ang2_val, ..., angn_val]`
+	# <br>	-- An array of relative distances (in the range `[0, 1]`)
+	# 	to the nearest obstacle for each angle outward from
+	# 	`center`.  This starts from angle 0 and increases in
+	# 	increments of the `degree_step` of the `Radar`, so
+	# 	`output[i]` corresponds to angle `i * degree_step`. The
+	# 	size of the output is then equal to
+	# 	`floor(360/degree_step)`.
+	#
 	def ScanRadar(self, center, grid_data):
 
 		radar_data = np.ones(int(360 / int(self._degree_step)))
@@ -38,6 +86,11 @@ class Radar:
 		return radar_data
 
 
+	## Sets the degree step of the `Radar`.
+	#
+	# @param newDegreeStep (float)
+	# <br>	-- the new degree step
+	#
 	def set_degree_step(self, newDegreeStep):
 		self._degree_step = newDegreeStep;
 		# Recalculate beams
@@ -50,7 +103,7 @@ class Radar:
 			currentStep += 1
 
 
-	def get_dynobs_data_index_range(self, scan_center, dynobs):
+	def _get_dynobs_data_index_range(self, scan_center, dynobs):
 		angle_range = [0, 360];
 		if dynobs.shape == 1:
 			angle_range = Geometry.circle_circle_overlap_angle_range(scan_center, self.radius, dynobs.coordinate, dynobs.radius);
@@ -66,13 +119,33 @@ class Radar:
 		return [int(index1), int(index2)]
 
 
+	## Produces a radar scan, ignoring static obstacles and including
+	# only dynamic obstacles.
+	#
+	# 
+	# @param center (numpy array)
+	# <br>	Format `[x, y]`
+	# <br>	-- The center point of the scan
+	#
+	# @param grid_data UNUSED
+	#
+	# @returns (numpy array)
+	# <br>	Format: `[ang1_val, ang2_val, ..., angn_val]`
+	# <br>	-- An array of relative distances (in the range `[0, 1]`)
+	# 	to the nearest dynamic obstacle for each angle outward from
+	# 	`center`. This starts from angle 0 and increases in
+	# 	increments of the `degree_step` of the `Radar`, so
+	# 	`output[i]` corresponds to angle `i * degree_step`. The
+	# 	size of the output is then equal to
+	# 	`floor(360/degree_step)`.
+	#
 	def scan_dynamic_obstacles(self, center, grid_data):
 		nPoints = self._nPoints
 		beams = self._beams
 		radar_data = np.ones([nPoints], dtype=np.float64);
 		sub_dynobs_list = [];
 		for dynobs in self.env.dynamic_obstacles:
-			index_range = self.get_dynobs_data_index_range(center, dynobs);
+			index_range = self._get_dynobs_data_index_range(center, dynobs);
 			if index_range is None:
 				continue;
 			for i in np.arange(index_range[0], index_range[1], 1):
@@ -97,6 +170,22 @@ class Radar:
 		return radar_data;
 
 
+	## Gets the `DynamicObstacle` object corresponding to the nearest
+	# dynamic obstacle along the beam at the specified angle.
+	#
+	#
+	# @param center (numpy array)
+	# <br>	Format `[x, y]`
+	# <br>	-- The center point of the scan
+	#
+	# @param angle (float)
+	# <br>	-- The angle (in degrees)
+	#
+	# @returns (float)
+	# <br>	-- The relative distance to the nearest obstacle within the
+	# 	radar's range at the given angle. The value is between 0
+	# 	and 1.
+	#
 	def get_dynobs_at_angle(self, center, angle):
 		ang_in_radians = angle * np.pi / 180.0;
 		endpoint = center + np.array([np.sin(ang_in_radians), np.cos(ang_in_radians)], dtype=np.float64) * self.radius;
