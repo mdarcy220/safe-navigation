104c71bd7272ece12721bf210a47c175e85a094a
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-09-16 19:57:05 -0400

    Clean up DRRT a bit

--- NavigationAlgorithm/DynamicRrtNavAlgo.py
+++ NavigationAlgorithm/DynamicRrtNavAlgo.py
@@ -378,21 +378,3 @@ class Node:
 		if self.parent:
 			self.parent.decrementSize(size);
 
-
-class Stack:
-		"A container with a last-in-first-out (LIFO) queuing policy."
-		def __init__(self):
-				self.list = []
-
-		def push(self,item):
-				"Push 'item' onto the stack"
-				self.list.append(item)
-
-		def pop(self):
-				"Pop the most recently pushed item from the stack"
-				return self.list.pop()
-
-		def isEmpty(self):
-				"Returns true if the stack is empty"
-				return len(self.list) == 0
-
