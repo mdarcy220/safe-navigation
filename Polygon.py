f6a00f809085193ee7ec95ba137ae320d1100d3c
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-12-19 22:55:38 -0500

    Fix line wrapping

--- Polygon.py
+++ Polygon.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 
-## @package Human
+## @package Polygon
 #
 
 from Shape import Shape
 import numpy as np
 import Geometry
-import math
 
 class Polygon(Shape):
-	def __init__(self, Points):
-		"""
-		A polygon is defined as a list of points
-		Each side of the polygon is the connection between two 
-		consecutive points from the list
-		with the last connection be the one between the last and first point
+	## A polygon is defined as a list of points Each side of the
+	# polygon is the connection between two consecutive points from the
+	# list with the last connection be the one between the last and first
+	# point
+	# 
+	# The polygon internal part of the polygon is considered to be to the
+	# right of each line
+	# 
+	# @param vertices (array-like)
+	# Format: `[[x1, y1], [x1, y2], ..., [xn, yn]]`
+	# <br>  -- list containing the x and y coordinates of the vertices
+	#
+	def __init__(self, vertices):
+		self._vertices = np.array(vertices)
 
-		The polygon internal part of the polygon 
-		is considered to be to the right of each line
-
-		Parameters:
-		Points -- list containing the x and y coordinates of the vertices
-		"""
-
-		self.Points = Points;
 
 	## Get the intersection point(s) of this polygon with the given line
 	#  which is the intersection of each edge of the polygon with the
@@ -38,16 +37,16 @@ class Polygon(Shape):
 		given line.
 		"""
 		intersections = []
-		for i in range(len(self.Points)):
-			edge = [self.Points[i-1],self.Points[i]];
-			intersection = Geometry.line_line_intersection(line,edge);
+		for i in range(len(self._vertices)):
+			edge = [self._vertices[i-1],self._vertices[i]];
+			intersection = Geometry.line_line_intersection(line, edge);
 			if intersection is not None:
 				intersections.append(list(intersection));
 		return intersections;
 
 
 	def __repr__(self):
-		return "Polygon([])".format(self.Points);
+		return "Polygon([])".format(self._vertices);
 
 
 
