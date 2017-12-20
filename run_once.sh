81b5ce7748ca776fa7e168d15764014eae7136da
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-12-19 21:11:30 -0500

    Fix performance profile output to SVG

--- run_once.sh
+++ run_once.sh
@@ -9,7 +9,7 @@ simulator_args+=( --radar-range 100 );
 simulator_args+=( --radar-resolution 5 );
 simulator_args+=( --batch-mode );
 simulator_args+=( --max-steps 5000 );
-simulator_args+=( --max-fps 4 );
+simulator_args+=( --max-fps 0 );
 simulator_args+=( --robot-movement-momentum 0.0 );
 simulator_args+=( --speed-mode 0 );
 simulator_args+=( --map-modifier-num 13 );
@@ -21,5 +21,6 @@ simulator_args+=( --display-every-frame );
 
 time python3 ${python_args[@]} Main.py ${simulator_args[@]};
 
-gprof2dot -f pstats /tmp/safenav_profile1.profile -o /tmp/safenav_profile1.dot | dot -Tsvg -o callgraph.svg
+gprof2dot -f pstats /tmp/safenav_profile1.profile -o /tmp/safenav_profile1.dot;
+dot -T svg /tmp/safenav_profile1.dot > /tmp/safenav_profile1.svg;
 
