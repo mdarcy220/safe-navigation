e2544d55733ab5028a5d7e1f343df771f8a38aef
MahmoudHamandi <mahmoudhamandi@gmail.com>
2018-01-04 17:21:39 -0500

    Addition of target into action prediction network

--- direction_predicter/model.py
+++ direction_predicter/model.py
@@ -21,10 +21,12 @@ import cntk as C
 
 class action_prediction:
 	
-	def __init__(self, feature_vector, action_vector, learning_rate, name='action_predicter'):
-		self._input_size = (feature_vector[0]+1,feature_vector[1])
+	def __init__(self, feature_vector, target_vector, action_vector, learning_rate, name='action_predicter'):
+		self._input_size  = (feature_vector[0]+1,feature_vector[1])
 		self._output_size = action_vector
+		self._target_size = target_vector
 		self._input = C.sequence.input_variable(self._input_size)
+		self._target = C.sequence.input_variable(self._target_size)
 		self._output = C.sequence.input_variable(self._output_size)
 		self.name = name
 		self._batch_size = 8
@@ -34,7 +36,7 @@ class action_prediction:
 		self._predicted = {}
 
 	def create_model(self):
-		model = C.layers.Sequential([
+		modeli = C.layers.Sequential([
 		# Convolution layers
 		C.layers.Convolution2D((1,3), num_filters=8, pad=True, reduction_rank=0, activation=C.ops.tanh),
 		C.layers.Convolution2D((1,3), num_filters=16, pad=True, reduction_rank=1, activation=C.ops.tanh),
@@ -43,14 +45,25 @@ class action_prediction:
 		# Dense layers
 		C.layers.Dense(128, activation=C.ops.relu),
 		C.layers.Dense(64, activation=C.ops.relu),
-		C.layers.Dense(32, activation=C.ops.relu),
+		C.layers.Dense(32, activation=C.ops.relu)
+		])(self._input)
+		### target
+		modelt = C.layers.Sequential([
+		C.layers.Dense(32, activation=C.ops.relu,name='dense1')
+		]) (self._target)
+		### concatenate both processed target and observations
+		inputs = C.ops.splice(modeli,modelt)
+		### Use input to predict next hidden state, and generate
+		### next observation
+		model = C.layers.Sequential([
 		######
+		C.layers.Dense(32, activation=C.ops.relu,name='dense1'),
 		# Recurrence
 		C.layers.Recurrence(C.layers.LSTM(32, init=C.glorot_uniform())),
 		######
 		# Prediction
-		C.layers.Dense(32, activation=C.ops.softmax)
-		])(self._input)
+		C.layers.Dense(32, activation=None)
+		])(inputs)
 		
 		loss = C.cross_entropy_with_softmax(model, self._output)
 		error = C.classification_error(model, self._output)
@@ -60,50 +73,56 @@ class action_prediction:
 		trainer = C.Trainer(model, (loss,loss), learner, progress_printer)
 		return model, loss, learner, trainer
 
-	def train_network(self, data, actions):
-		self.predict(data)
+	def train_network(self, data, targets, actions):
+		self.predict(data,targets)
 		for i in range(self._max_iter):
-			input_sequence,output_sequence = self.sequence_minibatch(data, actions,self._batch_size)
-			self._trainer.train_minibatch({self._input: input_sequence, self._output: output_sequence})
+			input_sequence,target_sequence,output_sequence = self.sequence_minibatch(data, targets, actions,self._batch_size)
+			self._trainer.train_minibatch({self._input: input_sequence, self._target: target_sequence, self._output: output_sequence})
 			self._trainer.summarize_training_progress()
 			if i%10 == 0:
 				self._model.save('action_predicter.dnn')
 
-	def sequence_minibatch(self, data, actions, batch_size):
+	def sequence_minibatch(self, data, targets, actions, batch_size):
 		sequence_keys    = list(data.keys())
 		minibatch_keys   = random.sample(sequence_keys,batch_size)
 		minibatch_input  = []
+		minibatch_target = []
 		minibatch_output = []
 
 		for key in minibatch_keys:
-			_input,_ouput = self.input_output_sequence(data,actions,key)
+			_input,_target,_ouput = self.input_output_sequence(data,targets,actions,key)
 			minibatch_input.append(_input)
+			minibatch_target.append(_target)
 			minibatch_output.append(_ouput)
 		
-		return minibatch_input,minibatch_output
+		return minibatch_input,minibatch_target,minibatch_output
 	
-	def input_output_sequence(self, data, actions, seq_key):
+	def input_output_sequence(self, data, targets, actions, seq_key):
 		data_k = data[seq_key]
 		input_sequence = np.zeros((len(data_k)-2,self._input_size[0],self._input_size[1]), dtype=np.float32)
+		target_sequence = np.zeros((len(data_k)-2,self._target_size[0],self._target_size[1]), dtype=np.float32)
 		output_sequence = np.zeros((len(data_k)-2,self._output_size[0],self._output_size[1]), dtype=np.float32)
+		
 		for i in range(0,len(data_k)-2):
 			input_sequence[i,0,:] = data_k[i]
 			input_sequence[i,1,:] = data_k[i+1]
 			input_sequence[i,2,:] = self._predicted[seq_key][i+2]
+			target_sequence[i,:,:] = targets[seq_key][i]
 			output_sequence[i,0,:] = actions[seq_key][i+2]
-		return input_sequence,output_sequence
+		return input_sequence,target_sequence,output_sequence
 	
-	def predict(self, data):
+	def predict(self, data, targets):
 		sequence_keys = list(data.keys())
 		feature_predicter = C.load_model('feature_predicter.dnn')
 		k = 1
 		for key in sequence_keys:
 			self._predicted[key] = [0,0]
 			data_k = data[key]
+			target_k = targets[key]
 
 			for i in range(0,len(data_k)-2):
 				input_vect = np.zeros((self._input_size[0]-1,self._input_size[1]), dtype=np.float32)
 				input_vect[0,:] = data_k[i]
 				input_vect[1,:] = data_k[i+1]
-				self._predicted[key].append(feature_predicter(input_vect)[0])
+				self._predicted[key].append(feature_predicter(input_vect,target_k[i])[0])
 
--- direction_predicter/test.py
+++ direction_predicter/test.py
@@ -13,7 +13,11 @@ from model import action_prediction
 import json
 
 
-f1 = action_prediction((2,360),(1,32),0.5)
+#######################
+target_dist = 30
+target_var = 5
+#######################
+f1 = action_prediction((2,360),(1,32),(1,32),0.5)
 with open('human_observations.json') as json_data:
 	data = json.load(json_data)
 #print(np.array(data[list(data.keys())[0]]['radardata_list'][0]['observation']).shape)
@@ -21,18 +25,43 @@ with open('human_observations.json') as json_data:
 #print(data.keys())
 data_new = {}
 actions  = {}
+targets = {}
 for key in data.keys():
 	data_new[key] = []
 	actions[key] = []
+	targets[key] = []
 	observations = np.array(data[key]['radardata_list'])
+	n = len(observations)
 	for i in range(len(observations)):
 		observation = np.array(observations[i]['observation'], dtype=np.float32)
 		data_new[key].append(observation)
-		
+		### compute action list ###
 		velocity = np.array(observations[i]['vel'], dtype=np.float32)
 		angle    = math.atan2(velocity[1],velocity[0])
+		angle    = math.degrees(angle)
+		angle    = (angle+360) % 360
 		action   = np.zeros((1,32),dtype=np.float32)
 		the_angle = int(round(angle*31/360))
-		action[the_angle] = 1
+		action[0,the_angle] = 1
 		actions[key].append(action)
-f1.train_network(data_new, actions)
+		### compute target list ###
+		target_position = np.zeros((1,2))
+		if i+target_dist+target_var < n:
+			for j in range(i+target_dist,i+target_dist+target_var):
+				target_position += np.array(observations[j]['position'])
+			target_position = target_position/target_var
+			#target_position[0] = -target_position[0]/target_var
+			#target_position[1] = -target_position[1]/target_var
+		else:
+			target_position += np.array(observations[n-1]['position'])
+		target_direction = target_position - np.array(observations[i]['position'])
+		target_angle    = math.atan2(target_direction[0,1],target_direction[0,0])
+		target_angle    =  math.degrees(target_angle)
+		target_angle    = (target_angle+360) % 360
+		target_action   = np.zeros((1,32),dtype=np.float32)
+		target_angle    = int(round(target_angle*31/360))
+		target_action[0,target_angle] = 1
+		targets[key].append(target_action)
+
+
+f1.train_network(data_new, targets, actions)
