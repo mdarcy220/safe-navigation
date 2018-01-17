dfcc5993b0e61782eb0bbfb875dbc4799a1e5657
MahmoudHamandi <mahmoudhamandi@gmail.com>
2018-01-17 10:45:51 -0500

    Implementation of the GRP test with velocity

--- GRP/complete_model.py
+++ GRP/complete_model.py
@@ -21,13 +21,15 @@ import cntk as C
 
 class action_prediction:
 	
-	def __init__(self, feature_vector, target_vector, action_vector, learning_rate, name='action_predicter'):
-		self._input_size  = feature_vector
-		self._output_size = action_vector
-		self._target_size = target_vector
-		self._input = C.input_variable(self._input_size)
-		self._target = C.input_variable(self._target_size)
-		self._output = C.input_variable(self._output_size)
+	def __init__(self, feature_vector, target_vector, action_vector, velocity, learning_rate, name='action_predicter'):
+		self._input_size    = feature_vector
+		self._output_size   = action_vector
+		self._target_size   = target_vector
+		self._velocity_size = velocity
+		self._input           = C.input_variable(self._input_size)
+		self._target          = C.input_variable(self._target_size)
+		self._output          = C.input_variable(self._output_size)
+		self._output_velocity = C.input_variable(self._velocity_size)
 		self.name = name
 		self._batch_size = 8
 		self._max_iter = 1000000
@@ -37,7 +39,7 @@ class action_prediction:
 		self._predicted = {}
 
 	def create_model(self):
-		action_model = C.load_model('action_predicter.dnn')(self._input,self._target)
+		action_model = C.load_model('GRP.dnn')(self._input,self._target)
 		action_model = action_model.clone(C.CloneMethod.freeze)
 		print(action_model)
 		node_outputs = C.logging.get_node_outputs(action_model)
@@ -51,64 +53,75 @@ class action_prediction:
 		#trainer = C.Trainer(action_model, (loss,loss), learner)
 		return action_model#, loss, learner, trainer
 
-	def test_network(self, data, targets, actions):
+	def test_network(self, data, targets, actions, velocities):
 		count   = 0
 		error   = 0
 		error_2 = 0
+		v_error = 0
 		for key in data.keys():
-			cn,er,er_2 = self.test_seq(data,targets,actions,key)
+			cn,er,er_2,v_er = self.test_seq(data,targets,actions,velocities,key)
 			error   += er
 			error_2 += er_2
 			count   += cn
 		print ('average classifiaction error:', error/count, 'for:', count, ' total steps')
 		print ('average angle classifiaction error:', error_2/count, 'for:', count, ' total steps', 'with angle', 180*math.acos(1-(error_2/count))/math.pi)
-
-	def test_seq(self, data, targets, actions, key):
-		input_sequence,target_sequence,output_sequence = self.sequence_batch(data, targets, actions, key)
+		print ('rmse normalized velocity error:', math.sqrt(v_error/count)/self._max_velocity, 'for:', count, ' total steps')
+		return cl_error/count, cl_error_2/count
+	
+	def test_seq(self, data, targets, actions, velocities, key):
+		input_sequence,target_sequence,output_sequence,velocity_sequence = self.sequence_batch(data, targets, actions, velocities, key)
 		predicted_values = self._model.eval({
 			self._model.arguments[0]: input_sequence, 
 			self._model.arguments[1]:target_sequence,
 			})
-		predicted_actions = []
+		predicted_actions  = []
+		predicted_velocity = []
 		for value in predicted_values:
+			direction = value[0:32]
+			velocity  = value[32]
 			action = np.zeros(32)
-			action[np.argmax(value)] = 1
+			action[np.argmax(direction)] = 1
 			predicted_actions.append(action)
+			predicted_velocity.append(velocity)
 		count   = 0
 		error   = 0
 		error_2 = 0
+		v_error = 0
 		for i in range(0,len(predicted_values)):
 			pre_cl  = np.where(predicted_actions[i] == 1)[0]
 			real_cl = np.where(output_sequence[i].flatten() == 1)[0]
 			error   += 0 if pre_cl == real_cl else 1 
 			error_2 += abs(1 - math.cos((max(real_cl,pre_cl) - min(real_cl,pre_cl))*math.pi/32))
-
-			#error += sum(abs(sum(np.array(predicted_actions[i]) 
-			#    - np.array(output_sequence[i]))))/2.0
+			v_error += np.power(predicted_velocity[i] - 
+			    velocity_sequence[i],2)
 			count += 1
-		return count, error, error_2
+		return count, error, error_2, v_error
 
-	def sequence_batch(self, data, targets, actions, key):
+	def sequence_batch(self, data, targets, actions, vel, key):
 		batch_input  = []
 		batch_target = []
 		batch_output = []
-		_input,_target,_ouput = self.input_output_sequence(data,targets,actions,key)
+		batch_veloc  = []
+		_input,_target,_ouput,_vel = self.input_output_sequence(data,targets,actions,vel,key)
 		for i in range(len(_input)):
 			batch_input.append(_input[i])
 			batch_target.append(_target[i])
 			batch_output.append(_ouput[i])
+			bathc_veloc.append(_vel[i])
 		
-		return batch_input,batch_target,batch_output
+		return batch_input,batch_target,batch_output,bathc_veloc
 	
-	def input_output_sequence(self, data, targets, actions, seq_key):
+	def input_output_sequence(self, data, targets, actions, vel, seq_key):
 		data_k = data[seq_key]
 		input_sequence = np.zeros((len(data_k),self._input_size[0],self._input_size[1]), dtype=np.float32)
 		target_sequence = np.zeros((len(data_k),self._target_size[0],self._target_size[1]), dtype=np.float32)
 		output_sequence = np.zeros((len(data_k),self._output_size[0],self._output_size[1]), dtype=np.float32)
+		vel_sequence    = np.zeros((len(data_k),self._velocity_size[0],self._velocity_size[1]), dtype=np.float32)
 		
 		for i in range(0,len(data_k)):
-			input_sequence[i,0,:] = data_k[i]
+			input_sequence[i,0,:]  = data_k[i]
 			target_sequence[i,:,:] = targets[seq_key][i]
 			output_sequence[i,0,:] = actions[seq_key][i]
-		return input_sequence,target_sequence,output_sequence
+			vel_sequence[i,0,:]    = vel[seq_key][i]
+		return input_sequence,target_sequence,output_sequence,vel_sequence
 	
--- GRP/test.py
+++ GRP/test.py
@@ -17,7 +17,7 @@ import json
 target_dist = 30
 target_var = 5
 #######################
-f1 = action_prediction((1,360),(1,360),(1,32),0.5)
+f1 = action_prediction((1,360),(1,360),(1,32),(1,32),0.5)
 with open('../feature_predicter/testing_human_data.json') as json_data:
 	data = json.load(json_data)
 #print(np.array(data[list(data.keys())[0]]['radardata_list'][0]['observation']).shape)
@@ -25,11 +25,13 @@ with open('../feature_predicter/testing_human_data.json') as json_data:
 #print(data.keys())
 data_new = {}
 actions  = {}
-targets = {}
+targets  = {}
+vel      = {}
 for key in data.keys():
 	data_new[key] = []
-	actions[key] = []
-	targets[key] = []
+	actions [key] = []
+	targets [key] = []
+	vel     [key] = []
 	observations = np.array(data[key]['radardata_list'])
 	n = len(observations)
 	for i in range(len(observations)):
@@ -44,6 +46,13 @@ for key in data.keys():
 		the_angle = int(round(angle*31/360))
 		action[0,the_angle] = 1
 		actions[key].append(action)
+
+		veloc  = np.zeros((1,1))
+		velo   = np.sqrt(np.sum(np.power(velocity,2)))
+		velo   = velo if velo < max_velocity else max_velocity
+		veloc += velo/max_velocity
+		vel[key].append(veloc)
+
 		### compute target list ###
 		target_position = np.zeros((1,360))
 		if i+target_dist+target_var < n:
@@ -56,4 +65,4 @@ for key in data.keys():
 			target_position += np.array(observations[n-1]['observation'])
 		targets[key].append(target_position)
 
-f1.test_network(data_new,targets,actions)
+f1.test_network(data_new,targets,actions,vel)
