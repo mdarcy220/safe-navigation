367369abe153188c84d94e55a230ba9c566fe5dc
Mike D'Arcy <m.darcy.220@gmail.com>
2018-03-01 13:58:12 -0500

    Increase event generation

--- EventMap.py
+++ EventMap.py
@@ -1,6 +1,8 @@
 
 
 import numpy as np
+import random
+import Vector
 
 ## @package EventMap
 #
@@ -19,8 +21,9 @@ class EventMap:
 	# <br>  Format: `[[x, y], [width, height]]`
 	# <br>  -- Rectangle defining the area that events can occur
 	#
-	def __init__(self, map_rect):
+	def __init__(self, map_rect, event_points):
 		self._map_rect = map_rect
+		self._event_points = event_points
 		self._location = self._map_rect[0]
 		self._dimensions = self._map_rect[1]
 		self._events = set()
@@ -37,13 +40,17 @@ class EventMap:
 
 	def step(self, timestep):
 		self._step_num += timestep
-		event_x = np.random.uniform(self._dimensions[0]/2) + self._location[0]
-		event_y = np.random.uniform(self._dimensions[1]/2) + self._location[1]
-		self._events.add(Event([event_x, event_y], self._step_num))
+		#event_x = np.random.uniform(self._dimensions[0]/2) + self._location[0]
+		#event_y = np.random.uniform(self._dimensions[1]/2) + self._location[1]
+		for i in range(11):
+			event_loc = random.sample(self._event_points, 1)[0]
+			while Vector.distance_between(event_loc, [735, 150]) > 120:
+				event_loc = random.sample(self._event_points, 1)[0]
+			self._events.add(Event(event_loc, self._step_num))
 
 
 	def draw(self, dtool):
-		dtool.set_color((30, 30, 120))
+		dtool.set_color((30, 30, 220))
 		dtool.set_stroke_width(0);
 		for event in self._events:
 			dtool.draw_circle((event.location[0],event.location[1]), 2)
