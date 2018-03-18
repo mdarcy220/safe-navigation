e25b97699f667a45ad3a4367765444c03ed78981
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2018-03-18 18:38:51 -0400

    Fix indentation for Cython compile

--- NavigationAlgorithm/SFMNavAlgo.py
+++ NavigationAlgorithm/SFMNavAlgo.py
@@ -75,13 +75,13 @@ class SFMNavigationAlgorithm(AbstractNavigationAlgorithm):
 		v_0 = self._get_target_direction_input_data()
 		v_0 = v_0*0.306/math.sqrt(np.sum(np.power(v_0,2)))
 		if self.count == 0:
-		    v = 0.05*Vector.unit_vec_from_radians(Vector.radians_between(location, self._target.position))
-		    theta = math.atan2(v[1],v[0])
-		    self._old_position = self._start_pos
+			v = 0.05*Vector.unit_vec_from_radians(Vector.radians_between(location, self._target.position))
+			theta = math.atan2(v[1],v[0])
+			self._old_position = self._start_pos
 		else:
-		    v = location - self._old_position
-		    theta = math.atan2(v[1],v[0])
-		    self._old_position = location
+			v = location - self._old_position
+			theta = math.atan2(v[1],v[0])
+			self._old_position = location
 		force = self.force(v_0,v,np.array([location[0],location[1],theta]) ,humans,obstacles)
 		#force = force*0.306/math.sqrt(np.sum(np.power(force,2)))
 		force_norm = math.sqrt(np.sum(np.power(force,2)))
@@ -96,7 +96,7 @@ class SFMNavigationAlgorithm(AbstractNavigationAlgorithm):
 
 
 	def _get_target_direction_input_data(self):
-		target_vec    = np.subtract(self._target.position, self._gps.location())
+		target_vec	= np.subtract(self._target.position, self._gps.location())
 		return target_vec
 
 
@@ -110,24 +110,24 @@ class SFMNavigationAlgorithm(AbstractNavigationAlgorithm):
 		self._min_cur_radar_scans = min(np.min(radar_data), self._min_cur_radar_scans)
 		objects = list(set(data_obj))
 		if None in objects:
-		    objects.remove(None)
+			objects.remove(None)
 		data = []
 		data_vect = [i for i,x in enumerate(data_obj) if x != None]
 		for obj in objects:
-		    data_object = {}
-		    the_min  = -1
-		    min_value = float('inf')
-		    for i in data_vect:
-		      if data_obj[i]._obs_id != obj._obs_id:
-		        continue
-		      if radar_data[i] <  min_value:
-		        min_value = radar_data[i]
-		        the_min = i
+			data_object = {}
+			the_min  = -1
+			min_value = float('inf')
+			for i in data_vect:
+				if data_obj[i]._obs_id != obj._obs_id:
+					continue
+				if radar_data[i] <  min_value:
+					min_value = radar_data[i]
+					the_min = i
 
-		    #the_min = np.argmin([radar_data[i] for i in data_vect if data_obj[i]._obs_id == obj._obs_id])
-		    data_object['distance'] = radar_data[the_min] 
-		    data_object['position'] = intersections[:,the_min]
-		    data.append(data_object)
+			#the_min = np.argmin([radar_data[i] for i in data_vect if data_obj[i]._obs_id == obj._obs_id])
+			data_object['distance'] = radar_data[the_min] 
+			data_object['position'] = intersections[:,the_min]
+			data.append(data_object)
 		return data
 
 	def _create_human_data(self):
@@ -136,55 +136,55 @@ class SFMNavigationAlgorithm(AbstractNavigationAlgorithm):
 		self._min_cur_radar_scans = min(np.min(radar_data), self._min_cur_radar_scans)
 		objects = list(set(data_obj))
 		if None in objects:
-		    objects.remove(None)
+			objects.remove(None)
 		data = []
 		data_vect = [i for i,x in enumerate(data_obj) if x != None]
 		for obj in objects:
-		    data_object = {}
+			data_object = {}
 
-		    data_object['distance'] = min([radar_data[i] for i in data_vect if data_obj[i]._obs_id == obj._obs_id])
-		    data_object['position'] = obj.coordinate
-		    data.append(data_object)
+			data_object['distance'] = min([radar_data[i] for i in data_vect if data_obj[i]._obs_id == obj._obs_id])
+			data_object['position'] = obj.coordinate
+			data.append(data_object)
 
 		return data
 
 	def force(self,v_0, v, pose, humans, objs):
-	    """
-	    Parameters:
-	    v_0 -- a numpy array of the shape (2x1) or (2,)
-	    v   -- a numpy array of the shape (2x1) or (2,)
-	    pose -- tuple or list of the shape (x,y,theta) or [x,y,theta]
-	    humans -- list of objects (currently should be ellipses)
-	    obstacles -- list of arbitraty objects
-	    object -- object instantiation.
-	    """
-	    force = np.zeros((2,))
-	    # force to target:
-	    force_to_target = self._alpha*self._kappa *(v_0-v)
-	    for human in humans:
-		    pos = human['position']
-		    dis = human['distance']
-		    w = self.weight(pose,pos)
-		    f = self._A * math.exp(self._d - dis)/self._B
-		    direction = pos - pose[0:2]
-		    direction = direction /math.sqrt(np.sum(np.power(direction,2)))
-		    force -= self._gamma*f*direction*w
-	    for obj in objs:
-		    pos = obj['position']
-		    dis = obj['distance']
-		    w = self.weight(pose,pos)
-		    f = self._A * math.exp(self._d - dis)/self._B
-		    direction = pos - pose[0:2]
-		    direction = direction /math.sqrt(np.sum(np.power(direction,2)))
-		    force -= self._delta*f*direction*w
-	    force += force_to_target
+		"""
+		Parameters:
+		v_0 -- a numpy array of the shape (2x1) or (2,)
+		v   -- a numpy array of the shape (2x1) or (2,)
+		pose -- tuple or list of the shape (x,y,theta) or [x,y,theta]
+		humans -- list of objects (currently should be ellipses)
+		obstacles -- list of arbitraty objects
+		object -- object instantiation.
+		"""
+		force = np.zeros((2,))
+		# force to target:
+		force_to_target = self._alpha*self._kappa *(v_0-v)
+		for human in humans:
+			pos = human['position']
+			dis = human['distance']
+			w = self.weight(pose,pos)
+			f = self._A * math.exp(self._d - dis)/self._B
+			direction = pos - pose[0:2]
+			direction = direction /math.sqrt(np.sum(np.power(direction,2)))
+			force -= self._gamma*f*direction*w
+		for obj in objs:
+			pos = obj['position']
+			dis = obj['distance']
+			w = self.weight(pose,pos)
+			f = self._A * math.exp(self._d - dis)/self._B
+			direction = pos - pose[0:2]
+			direction = direction /math.sqrt(np.sum(np.power(direction,2)))
+			force -= self._delta*f*direction*w
+		force += force_to_target
 
-	    return force
+		return force
 		
 	def weight(self,pose_0, pose_1):
-	    """
-	    returns the weight of the force based on its location
-	    """
-	    phi = abs(math.atan2(pose_1[1] - pose_0[1],pose_1[0]-pose_0[0]) - pose_0[2])
-	    weight = self._l +(1-self._l)*(1+math.cos(phi))/2
-	    return weight
+		"""
+		returns the weight of the force based on its location
+		"""
+		phi = abs(math.atan2(pose_1[1] - pose_0[1],pose_1[0]-pose_0[0]) - pose_0[2])
+		weight = self._l +(1-self._l)*(1+math.cos(phi))/2
+		return weight
